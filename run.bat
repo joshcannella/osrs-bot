d64783feec16a94dbc896f2d42e1c12733523701
joshcannella <joshcannella@users.noreply.github.com>
2026-03-02 16:35:59 -0500

    run.bat: make browser launch optional with --launch-browser flag

--- run.bat
+++ run.bat
@@ -1,11 +1,13 @@
 @echo off
 REM Pull latest and launch ChromaScape
-REM Double-click this file or run from PowerShell
+REM Usage: run.bat [--launch-browser]
 
 cd /d "%~dp0"
 git pull
 git submodule update --init --recursive
 
 cd ChromaScape
-start http://localhost:8080
+
+if "%1"=="--launch-browser" start http://localhost:8080
+
 call gradlew.bat bootRun
