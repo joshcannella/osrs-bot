19e4b5b455e04e989e5176250cbcd50a8df169d4
joshcannella <joshcannella@users.noreply.github.com>
2026-03-02 16:34:32 -0500

    run.bat: auto-open browser and pull submodules

--- run.bat
+++ run.bat
@@ -4,6 +4,8 @@ REM Double-click this file or run from PowerShell
 
 cd /d "%~dp0"
 git pull
+git submodule update --init --recursive
 
 cd ChromaScape
+start http://localhost:8080
 call gradlew.bat bootRun
