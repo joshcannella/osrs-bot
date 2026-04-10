f1a98724e9502b97b4a30b0584c840906a0c9c85
joshcannella <joshcannella@users.noreply.github.com>
2026-04-10 00:03:11 -0400

    deploy: 20260410-0003

--- dreambot/src/main/java/scripts/brutusfighter/BrutusFighterScript.java
+++ dreambot/src/main/java/scripts/brutusfighter/BrutusFighterScript.java
@@ -11,7 +11,7 @@ import scripts.shared.antiban.AntiBanNode;
 
 import java.awt.*;
 
-@ScriptManifest(name = "brutus-fighter", author = "osrs-bot", version = 0.18,
+@ScriptManifest(name = "brutus-fighter", author = "osrs-bot", version = 0.19,
                 description = "Fights Brutus cow boss - dodges specials, eats, loots, banks",
                 category = Category.COMBAT)
 public class BrutusFighterScript extends TreeScript {
